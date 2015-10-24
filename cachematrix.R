9870e4e97105183183d7e13de08002051ddc5d77
ryanniemann <ryanniemann@hotmail.com>
2015-10-24 10:51:14 -0600

    Programming Assignment 2
    Updated commit for the assignment. Validated code and cleaned up
    comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,44 @@
-## The assignment is regarding lexical scoping and caching 
-## functions that may require a long computation time. 
+# The assignment is regarding lexical scoping and caching 
+# functions that may require a long computation time. 
 
-## This first function, makeCacheMatrix creates a special 
-## matrix object that can cache the input matrix and its inverse
+# This first function creates a special 
+# matrix object that can cache the input matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL # sets the value of m to NULL (provides a default if cacheSolve has not yet been used)
-        y <- NULL # sets the value of y to NULL (provides a default if cacheSolve has not yet been used)
-        setmatrix <- function(y) { #set the value of the matrix
-                x <<- y ## caches the inputted matrix so that cacheSolve can check whether it has changed (note this is within the setmatrix function)
-                m <<- NULL # # sets the value of m (the matrix inverse if used cacheSolve) to NULL
+        m <- NULL 
+        y <- NULL 
+        
+        setmatrix <- function(y) { 
+                x <<- y 
+                m <<- NULL 
         }
-        # Parts removed
-        list(setmatrix = setmatrix, getmatrix = getmatrix, # creates a list to house the four functions
+        getmatrix <- function() x 
+        setinverse <- function(inverse) m <<- inverse 
+        getinverse <- function() m 
+        
+        list(setmatrix = setmatrix, getmatrix = getmatrix, 
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
-## The second function cacheSolve calls functions stored 
-## in the special "matrix" returned by makeCacheMatrix
+# The following function returns the inverse of the matrix 
 
-cacheSolve <- function (x(), ...) {
-        # Need to compare matrix to what was there
-        m <- x$getinverse() # if an inverse has already been calculated this gets it
-        if(!is.null(m)){ # check to see if cacheSolve has been run before
-                if(x$setmatrix() == x$getmatrix()) { # check that matrix hasn't changed, and if it hasn't, sends a text message and returns the cached matrix
-                        #parts removed
-                        return(m)
-                }
-                # otherwise 
-                y <- x$getmatrix() # run the getmatrix function to get the value of the input matrix
-                x$setmatrix(y) # run the setmatrix function on the input matrix to cache it
-                m <- solve(y, ...) # compute the value of the inverse of the input matrix
-                x$setinverse(m) # run the setinverse function on the inverse to cache the inverse
-                m # return the inverse
+cacheSolve <- function (x, ...) {
+        # first check if the inverse has already been computed 
+        
+        m <- x$getinverse() 
+        if(!is.null(m)){ 
+                return(m)
         }
+                
+        # if needed compute the inverse 
+                
+        y <- x$getmatrix() 
+        x$setmatrix(y) 
+        m <- solve(y, ...) 
+        x$setinverse(m) 
+        m 
+}
+
+
+
